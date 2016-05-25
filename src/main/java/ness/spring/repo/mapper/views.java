ec1bc3b06006bf08ee916f980d76f0eacafc22c2
ilievieru <ilie_vieru_2010@yahoo.com>
2016-05-25 16:45:54 +0300

    object Mapper/ json serialize/

---
+++ src/main/java/ness/spring/repo/mapper/views.java
@@ -0,0 +1,15 @@
+package ness.spring.repo.mapper;
+
+/**
+ * Created by P3700664 on 5/25/2016.
+ */
+public class Views {
+
+    public static class UserInfoLog{
+
+    }
+
+    public static class AdminInfoLog {
+
+    }
+}
